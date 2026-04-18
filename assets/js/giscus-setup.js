a50ca61006fa61f7a30cf67b340ad89e06da6175
chen-z-b <zhibinchanchen@gmail.com>
2026-04-18 18:22:49 +0000

    Deploying to gh-pages from @ chen-z-b/chen-z-b.github.io@f6169c00dcb7652978c68b06a5f2e56adea8027c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "chen-z-b/github.io",
+    "data-repo": "chen-z-b/chen-z-b.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
